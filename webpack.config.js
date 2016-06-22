30326d5a2ac80eaa2435714de475298530a8e677
Doni Leung <doni@kudelabs.com>
2016-06-22 22:16:16 +0800

    separate loaders for font files

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,27 @@ var loaders = [
     exclude: /node_modules/
   },
   {
-    test: /\.(png|jpg|ttf|eot|svg|woff2|woff)$/,
+    test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/font-woff"
+  },
+  {
+    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/font-woff"
+  },
+  {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=application/octet-stream"
+  },
+  {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "file"
+  },
+  {
+    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url?limit=10000&mimetype=image/svg+xml"
+  },
+  {
+    test: /\.(png|jpg)$/,
     loader: 'url?limit=25000'
   },
 
